ad20121ee441c218f96cd272b62b47d7fba437a0
Miguel_Garcia <migueldariogq.mdg@gmail.com>
2024-07-29 14:24:41 +0200

    programaFichajes

---
+++ ProgramaFichajes/admin/admin.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body>
+    <header class="w-full h-10 bg-blue-500 flex justify-center items-center">
+        <nav>
+            <ul class="flex gap-5">
+                <li class="font-bold text-white hover:text-gray-200 cursor-pointer">Trabajadores</li>
+                <li class="font-bold text-white hover:text-gray-200 cursor-pointer">Incidencias</li>
+                <li class="font-bold text-white hover:text-gray-200 cursor-pointer">Horarios</li>
+            </ul>
+        </nav>
+    </header>
+    <aside class="w-1/5 h-full bg-gray-200">
+        <ul>
+            <li>
+                Trabajadores
+            </li>
+            <li>
+                Añadir nuevo trabajador
+            </li>
+        </ul>
+    </aside>
+</body>
+
+</html>
