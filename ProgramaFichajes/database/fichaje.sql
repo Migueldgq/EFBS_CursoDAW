7581bbde12bac13caf356654f1aebe811c4e768f
Miguel_Garcia <migueldariogq.mdg@gmail.com>
2024-08-01 14:17:45 +0200

    db

--- ProgramaFichajes/database/fichaje.sql
+++ ProgramaFichajes/database/fichaje.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 30-07-2024 a las 14:05:25
+-- Tiempo de generación: 01-08-2024 a las 14:17:03
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -38,42 +38,7 @@ CREATE TABLE `fichajesentrada` (
 --
 
 INSERT INTO `fichajesentrada` (`id_fichajeEntrada`, `hora_fichajeEntrada`, `id_trabajador`) VALUES
-(1, '2024-07-30 11:17:53', 1),
-(2, '2024-07-30 11:17:57', 1),
-(3, '2024-07-30 11:18:22', 1),
-(4, '2024-07-30 11:18:32', 1),
-(5, '2024-07-30 11:22:29', 1),
-(6, '2024-07-30 11:23:23', 1),
-(7, '2024-07-30 11:23:39', 1),
-(8, '2024-07-30 12:14:41', 0),
-(9, '2024-07-30 12:15:05', 1),
-(10, '2024-07-30 12:15:12', 1),
-(11, '2024-07-30 12:15:38', 1),
-(12, '2024-07-30 12:16:49', 1),
-(13, '2024-07-30 12:16:52', 1),
-(14, '2024-07-30 12:16:54', 1),
-(15, '2024-07-30 12:21:12', 1),
-(16, '2024-07-30 12:36:47', 1),
-(17, '2024-07-30 12:38:18', 1),
-(18, '2024-07-30 12:39:15', 1),
-(19, '2024-07-30 12:39:47', 1),
-(20, '2024-07-30 12:41:11', 1),
-(21, '2024-07-30 12:42:07', 1),
-(22, '2024-07-30 12:50:18', 1),
-(23, '2024-07-30 12:50:42', 1),
-(24, '2024-07-30 12:50:56', 1),
-(25, '2024-07-30 12:51:38', 1),
-(26, '2024-07-30 12:52:04', 1),
-(27, '2024-07-30 12:52:28', 1),
-(28, '2024-07-30 12:54:35', 1),
-(29, '2024-07-30 12:55:29', 1),
-(30, '2024-07-30 12:55:59', 1),
-(31, '2024-07-30 12:56:35', 1),
-(32, '2024-07-30 12:56:49', 1),
-(33, '2024-07-30 13:05:53', 1),
-(34, '2024-07-30 13:16:09', 1),
-(35, '2024-07-30 13:19:18', 1),
-(36, '2024-07-30 13:19:29', 1);
+(1, '2024-08-01 12:34:10', 1);
 
 -- --------------------------------------------------------
 
@@ -83,9 +48,16 @@ INSERT INTO `fichajesentrada` (`id_fichajeEntrada`, `hora_fichajeEntrada`, `id_t
 
 CREATE TABLE `fichajessalida` (
   `id_fichajeSalida` int(11) NOT NULL,
-  `hora_fichajeSalida` int(11) NOT NULL,
+  `hora_fichajeSalida` datetime NOT NULL DEFAULT current_timestamp(),
   `id_trabajador` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `fichajessalida`
+--
+
+INSERT INTO `fichajessalida` (`id_fichajeSalida`, `hora_fichajeSalida`, `id_trabajador`) VALUES
+(1, '2024-08-01 14:15:35', 1);
 
 -- --------------------------------------------------------
 
@@ -119,17 +91,27 @@ CREATE TABLE `incidencias` (
   `motivo_justificaciones` varchar(120) DEFAULT NULL,
   `justificada` tinyint(1) NOT NULL DEFAULT 0,
   `fecha_incidencia` datetime NOT NULL DEFAULT current_timestamp(),
-  `id_trabajador` int(11) NOT NULL
+  `id_trabajador` int(11) NOT NULL,
+  `archivoJustificante` varchar(250) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `incidencias`
 --
 
-INSERT INTO `incidencias` (`id_incidencia`, `motivo_justificaciones`, `justificada`, `fecha_incidencia`, `id_trabajador`) VALUES
-(1, NULL, 0, '2024-07-30 13:18:08', 1),
-(2, NULL, 0, '2024-07-30 13:19:18', 1),
-(3, NULL, 0, '2024-07-30 13:19:29', 1);
+INSERT INTO `incidencias` (`id_incidencia`, `motivo_justificaciones`, `justificada`, `fecha_incidencia`, `id_trabajador`, `archivoJustificante`) VALUES
+(1, 'Atasco', 1, '2024-07-30 13:18:08', 1, NULL),
+(2, 'atasco', 1, '2024-07-30 13:19:18', 1, NULL),
+(3, 'Atasco', 1, '2024-07-30 13:19:29', 1, NULL),
+(4, 'Atasco', 1, '2024-07-31 09:07:00', 1, NULL),
+(5, 'Personal', 1, '2024-08-01 09:26:38', 1, NULL),
+(6, 'Personal', 1, '2024-08-01 10:29:16', 1, NULL),
+(7, 'Atasco', 1, '2024-08-01 10:38:52', 1, 'justificanteentrevista.pdf'),
+(8, 'Atasco', 1, '2024-08-01 12:31:33', 1, NULL),
+(9, 'Personal', 1, '2024-08-01 12:33:00', 1, NULL),
+(10, 'Atasco', 1, '2024-08-01 12:33:29', 1, NULL),
+(11, 'Médico', 1, '2024-08-01 12:34:10', 1, 'gru.jpg'),
+(12, NULL, 0, '2024-08-01 14:00:07', 1, NULL);
 
 -- --------------------------------------------------------
 
@@ -162,7 +144,8 @@ CREATE TABLE `trabajadores` (
 
 INSERT INTO `trabajadores` (`id_trabajador`, `nom_trab`, `ape_trab`, `dni_trab`, `id_horario`) VALUES
 (1, 'Miguel ', 'Garcia', '32943897P', 1),
-(2, 'Edgar', 'Sanchez', '52647889J', 2);
+(2, 'Edgar', 'Sanchez', '52647889J', 2),
+(3, 'Marián', 'Polo', '32586897X', 3);
 
 --
 -- Índices para tablas volcadas
@@ -173,6 +156,12 @@ INSERT INTO `trabajadores` (`id_trabajador`, `nom_trab`, `ape_trab`, `dni_trab`,
 --
 ALTER TABLE `fichajesentrada`
   ADD PRIMARY KEY (`id_fichajeEntrada`);
+
+--
+-- Indices de la tabla `fichajessalida`
+--
+ALTER TABLE `fichajessalida`
+  ADD PRIMARY KEY (`id_fichajeSalida`);
 
 --
 -- Indices de la tabla `horarios`
@@ -200,7 +189,13 @@ ALTER TABLE `trabajadores`
 -- AUTO_INCREMENT de la tabla `fichajesentrada`
 --
 ALTER TABLE `fichajesentrada`
-  MODIFY `id_fichajeEntrada` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `id_fichajeEntrada` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `fichajessalida`
+--
+ALTER TABLE `fichajessalida`
+  MODIFY `id_fichajeSalida` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `horarios`
@@ -212,13 +207,13 @@ ALTER TABLE `horarios`
 -- AUTO_INCREMENT de la tabla `incidencias`
 --
 ALTER TABLE `incidencias`
-  MODIFY `id_incidencia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_incidencia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `trabajadores`
 --
 ALTER TABLE `trabajadores`
-  MODIFY `id_trabajador` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_trabajador` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
