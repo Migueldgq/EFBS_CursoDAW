57337959d9063892ac20d51079ad5304095d31b2
Miguel_Garcia <migueldariogq.mdg@gmail.com>
2024-08-22 14:21:08 +0200

    fix

--- ejercicioreservas/altaevento.php
+++ ejercicioreservas/altaevento.php
@@ -12,7 +12,7 @@ include "funciones.php";
 
 mkdir("./eventos/$name", 0777);
 $ruta = "./eventos/$name/$img";
-_uploaded_file($tempIMG, $ruta);
+move_uploaded_file($tempIMG, $ruta);
 
 
 InsertEvent($name, $desc, $img, $author, $date, $aforo);
