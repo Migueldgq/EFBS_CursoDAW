aa98ab7516f5c7749be83bd7dc6005e3c8687eba
Miguel_Garcia <migueldariogq.mdg@gmail.com>
2024-08-29 14:21:38 +0200

    fecha

--- examenes/Examen27agosto/getFacturas.php
+++ examenes/Examen27agosto/getFacturas.php
@@ -2,17 +2,25 @@
 
 include 'conexion.php';
 
+// Establecer la localización en español
+setlocale(LC_TIME, 'spanish');
+
 $getFacturas = "SELECT * FROM facturas";
 $ejecutar = $conexion->query($getFacturas);
 
 $response = array();
 
 foreach ($ejecutar as $factura) {
+    // Convertir la fecha a un timestamp
+    $timestamp = strtotime($factura['factura_date']);
+
+    // Formatear la fecha a 'd de F de Y' en español
+    $fechaFormateada = strftime('%d de %B de %Y', $timestamp);
+
     $response[] = array(
         'id' => $factura['factura_id'],
-        'fecha' => $factura['factura_date']
+        'fecha' => $fechaFormateada
     );
 }
 
 echo json_encode($response);
-
